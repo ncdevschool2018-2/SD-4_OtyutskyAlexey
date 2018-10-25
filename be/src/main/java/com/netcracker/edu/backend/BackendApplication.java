32cc16923075ba0df8b1b894b8d6da70ad5da59d
KingAlexeyOtutski <alexeyotutski@gmail.com>
2018-10-25 17:27:02 +0300

    updated project structure, adding more directories to modules

---
+++ be/src/main/java/com/netcracker/edu/backend/BackendApplication.java
@@ -0,0 +1,16 @@
+package com.netcracker.edu.backend;
+
+/**
+ * Created by Alexey on 25.10.2018.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
+
---
+++ fapi/src/main/java/com.netcracker.edu.fapi/FapiApplication.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Alexey on 25.10.2018.
+ */
+package com.netcracker.edu.fapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FapiApplication.class, args);
+    }
+}
+
